f65bde3610fefdcd5f3e2adce8fe8d96b7e31074
Nelson Huynh <nelsonhuynh@hotmail.com>
2021-02-12 22:06:33 -0500

    This is the first

---
+++ script.js
@@ -0,0 +1,41 @@
+let input = document.getElementById("input");
+
+function divide(x, y){
+  let result = x / y
+  return result;
+}
+function multiply(x, y){
+  let result = x * y
+  return result;
+}
+function subtract(x, y){
+  let result = x - y
+  return result;
+}
+function add(x, y){
+  let result = x + y
+  return result;
+}
+
+const operate = (opp, x, y) => {
+  input.innerHTML = ""
+}
+const display () => {
+
+}
+
+
+
+
+console.log(add(4,2));
+
+
+document.getElementById("clear").addEventListener('click', operate);
+document.getElementById().addEventListener('click', display);
+// document.getElementById("divide").addEventListener('click', black);
+// document.addEventListener('click', black);
+// document.addEventListener('click', black);
+// document.addEventListener('click', black);
+
+
+console.log("t");
